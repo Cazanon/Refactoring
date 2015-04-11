464a3d75f4e44906a1f8da1663f716f3e07a9b6f
cazanon <carlos.azanon@gmail.com>
2015-04-11 10:53:16 +0200

    Implementado doble despacho en la clase point para conseguir el correcto funcionamiento de includeInterval

--- src/intervals/OpenedPoint.java
+++ src/intervals/OpenedPoint.java
@@ -5,49 +5,45 @@ public class OpenedPoint extends Point{
 	OpenedPoint(double value) {
 		super(value);
 	}
-
+	
+	@Override
 	public boolean lessThan(double value){
 		return this.getValue() < value;
 	}
 	
+	@Override
 	public boolean greaterThan(double value){
 		return this.getValue() > value;
 	}
 
 	@Override
-	public boolean greaterThan(Point value) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean greaterThan(Point point) {
+		return point.greaterThan(this);
 	}
 
 	@Override
-	public boolean lessThan(Point value) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean lessThan(Point point) {
+		return point.lessThan(this);
 	}
 
 	@Override
 	public boolean lessThan(ClosedPoint point) {
-		// TODO Auto-generated method stub
-		return false;
+		return this.getValue() >= point.getValue();
 	}
 
 	@Override
 	public boolean greaterThan(ClosedPoint point) {
-		// TODO Auto-generated method stub
-		return false;
+		return this.getValue() <= point.getValue();
 	}
 
 	@Override
 	public boolean lessThan(OpenedPoint point) {
-		// TODO Auto-generated method stub
-		return false;
+		return this.getValue() >= point.getValue();
 	}
 
 	@Override
 	public boolean greaterThan(OpenedPoint point) {
-		// TODO Auto-generated method stub
-		return false;
+		return this.getValue() <= point.getValue();
 	}
 	
 }
